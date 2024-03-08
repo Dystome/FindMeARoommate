c23f625b7934441344eb57c6d031a15f66ae7e94
dystome <atienferra@gmail.com>
2024-03-08 20:43:27 +0100

    Business Layer

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using PracticalProject.DataLayer.Repositories;
 
 
 var rep = new StudentRepository();
-var users = rep._context.Students.ToList();
+var users = rep.GetAll();
 foreach (var user in users)
 {
     Console.WriteLine(user.Name + " " + user.Surname + " " + user.Address);
